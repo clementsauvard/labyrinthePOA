a33c954ede8d0050a91c55741532d8091a9368ca
ClementS <clement.sauvard@u-psud.fr>
2018-03-30 12:43:44 +0200

    Création gardien.cpp + macron

---
+++ LabyrintheDebProto/Gardien.cpp
@@ -0,0 +1,29 @@
+#include "Chasseur.h"
+#include "Gardien.h"
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+Gardien::Gardien (Labyrinthe* l,const char* modele) : Mover (100, 80, l, modele)
+{
+	
+}
+
+void Gardien::update (void) {
+	cout << _l -> _nwall << endl;
+}
+
+bool Gardien::move (double dx, double dy) { 
+	_x += dx;
+	_y += dy;
+	return true;
+}
+
+void Gardien::fire (int angle_vertical) {
+
+}
+
+bool Gardien::process_fireball (float dx, float dy) { 
+	return false; 
+}
