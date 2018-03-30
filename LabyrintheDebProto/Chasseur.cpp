2e949730415658527bfb9aff15a280206434522b
Joe De Oliveira <jdeoliv@a-018818.ups.u-psud.fr>
2018-03-30 09:57:26 +0200

    Chasseur modifie

--- LabyrintheDebProto/Chasseur.cpp
+++ LabyrintheDebProto/Chasseur.cpp
@@ -1,68 +1,105 @@
 #include "Chasseur.h"
+#include "Gardien.h"
+#include <stdlib.h>
 
 /*
- *	Tente un deplacement.
+ *  Constructeur.
  */
-
-bool Chasseur::move_aux (double dx, double dy)
-{
-	if (EMPTY == _l -> data ((int)((_x + dx) / Environnement::scale),
-							 (int)((_y + dy) / Environnement::scale)))
-	{
-		_x += dx;
-		_y += dy;
-		return true;
-	}
-	return false;
-}
-
-/*
- *	Constructeur.
- */
-
 Chasseur::Chasseur (Labyrinthe* l) : Mover (100, 80, l, 0)
 {
-	_hunter_fire = new Sound ("sons/hunter_fire.wav");
-	_hunter_hit = new Sound ("sons/hunter_hit.wav");
-	if (_wall_hit == 0)
-		_wall_hit = new Sound ("sons/hit_wall.wav");
+    life=20;
+    _hunter_fire = new Sound ("sons/hunter_fire.wav");
+    _hunter_hit = new Sound ("sons/hunter_hit.wav");
+    if (_wall_hit == 0)
+        _wall_hit = new Sound ("sons/hit_wall.wav");
 }
 
 /*
- *	Fait bouger la boule de feu (ceci est une exemple, à vous de traiter les collisions spécifiques...)
+ *  Tente un deplacement.
  */
+bool Chasseur::move_aux (double dx, double dy)
+{
+    if (EMPTY == _l -> data ((int)((_x + dx) / Environnement::scale),
+                            (int)((_y + dy) / Environnement::scale)))
+    {
+        _x += dx;
+        _y += dy;
+        return true;
+    }
+    return false;
+}
 
+/*
+ *  Fait bouger la boule de feu (ceci est une exemple, à vous de traiter les collisions spécifiques...)
+ */
 bool Chasseur::process_fireball (float dx, float dy)
 {
-	// calculer la distance entre le chasseur et le lieu de l'explosion.
-	float	x = (_x - _fb -> get_x ()) / Environnement::scale;
-	float	y = (_y - _fb -> get_y ()) / Environnement::scale;
-	float	dist2 = x*x + y*y;
-	// on bouge que dans le vide!
-	if (EMPTY == _l -> data ((int)((_fb -> get_x () + dx) / Environnement::scale),
-							 (int)((_fb -> get_y () + dy) / Environnement::scale)))
-	{
-		message ("Woooshh ..... %d", (int) dist2);
-		// il y a la place.
-		return true;
-	}
-	// collision...
-	// calculer la distance maximum en ligne droite.
-	float	dmax2 = (_l -> width ())*(_l -> width ()) + (_l -> height ())*(_l -> height ());
-	// faire exploser la boule de feu avec un bruit fonction de la distance.
-	_wall_hit -> play (1. - dist2/dmax2);
-	message ("Booom...");
-	return false;
+    // calculer la distance entre le chasseur et le lieu de l'explosion.
+    float   x = (_x - _fb -> get_x ()) / Environnement::scale;
+    float   y = (_y - _fb -> get_y ()) / Environnement::scale;
+    float   dist2 = x*x + y*y;
+    float   dmax2;
+    // on bouge que dans le vide!
+    
+    switch ( _l -> data ((int)((_fb -> get_x () + dx) / Environnement::scale),(int)((_fb -> get_y () + dy) / Environnement::scale)) )  
+    {
+        case EMPTY: 
+            message("LE VIDE");
+            return true;  
+            break;  
+        case 1:
+            message("UN MUR");  
+            dmax2 = (_l -> width ())*(_l -> width ()) + (_l -> height ())*(_l -> height ());  
+            _wall_hit -> play (1. - dist2/dmax2);
+            return false;
+            break;
+        case 2:
+            message("UNE CAISSE");
+            dmax2 = (_l -> width ())*(_l -> width ()) + (_l -> height ())*(_l -> height ());
+            _wall_hit -> play (1. - dist2/dmax2);
+            return false;
+            break;
+        case 3:  
+            message("UN GARDIEN");
+            dmax2 = (_l -> width ())*(_l -> width ()) + (_l -> height ())*(_l -> height ());
+            _wall_hit -> play (1. - dist2/dmax2);
+            return false;
+            break;
+        case 4:  
+            message("UN TRESOR");
+            dmax2 = (_l -> width ())*(_l -> width ()) + (_l -> height ())*(_l -> height ());  
+            _wall_hit -> play (1. - dist2/dmax2);
+            return false;
+            break;
+         default:  
+            message("wtfdidujustdidulilshit");
+            dmax2 = (_l -> width ())*(_l -> width ()) + (_l -> height ())*(_l -> height ());
+            _wall_hit -> play (1. - dist2/dmax2);
+            return false;
+    }
+    // collision...
+    // calculer la distance maximum en ligne droite.
+    
+    // faire exploser la boule de feu avec un bruit fonction de la distance.
+    
+//    life--;
+    
+    //message ("Booom...");
+
 }
 
-/*
- *	Tire sur un ennemi.
- */
 
+
+
+/*
+ *  Tire sur un ennemi.
+ */
 void Chasseur::fire (int angle_vertical)
 {
-	message ("Woooshh...");
-	_hunter_fire -> play ();
-	_fb -> init (/* position initiale de la boule */ _x, _y, 10.,
-				 /* angles de visée */ angle_vertical, _angle);
+    int p1 = ( ((rand() % 10) - 5)*((20-life))/20 );
+    int p2 = ( ((rand() % 10) - 5)*((20-life))/20 );
+    message ("I AM THE WHOOSH...  %d , %d",p1,life);
+    _hunter_fire -> play ();
+    _fb -> init (/* position initiale de la boule */ _x, _y, 10.,
+                /* angles de visée */ angle_vertical+p1, _angle+p2);
 }
